337748f82436bd066a2d6d91076525d507bc9029
blairyoung15 <bey99d@gmail.com>
2022-05-08 13:34:06 -0500

    upated tag.init with columns in tags.js in models

--- models/Tag.js
+++ models/Tag.js
@@ -7,6 +7,15 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
   },
   {
     // pass in our imported sequelize connection (the direct connection to our database)
