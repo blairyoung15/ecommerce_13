758da4e03bfb956b31a12a9dc5707e9350dae157
blairyoung15 <bey99d@gmail.com>
2022-05-08 13:21:59 -0500

    updated sync sequelize in server.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -11,11 +11,39 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+
+  User.findOne({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbProductData => {
+      if (!dbProductData) {
+        res.status(404).json({ message: 'No product found with this id' });
+        return;
+      }
+      res.json(dbProductData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
+
 
 router.post('/', (req, res) => {
   // create a new category
-});
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+    Category.create({
+      category_name: req.body.categoryname,
+    })
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
