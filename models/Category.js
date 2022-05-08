b8fa0b8ea7da3a31f4044fca521cd6037d76a7ab
blairyoung15 <bey99d@gmail.com>
2022-05-08 13:25:34 -0500

    updated category.init with columns in category.js in models

--- models/Category.js
+++ models/Category.js
@@ -7,6 +7,16 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },  
   },
   {
   // pass in our imported sequelize connection (the direct connection to our database)
